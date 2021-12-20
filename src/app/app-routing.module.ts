738ffa36f6582e37b69fd6259fc496395a6a898e
mateusdreher <mateusdreher.dev@gmail.com>
2021-12-20 16:16:36 -0300

    Initial Structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SchedulesModule } from './pages/schedules/schedules.module';
+
+const routes: Routes = [
+  { 
+    path: 'shedule', loadChildren: () => import('./pages/schedules/schedules.module').then(mod => mod.SchedulesModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
